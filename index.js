c6f85837fe426ae9e11a5b4feafe548ed613f43a
Mr.Tom <127591461+MrTom-js16@users.noreply.github.com>
2023-03-11 22:03:19 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// رفع لفل حسابات بروبوت
+const Discord = require("discord.js-selfbot-v13"); 
+const client = new Discord.Client({
+    intents: [Discord.Intents.FLAGS.GUILDS]
+});
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+const {userAccount} = require("sphinx-run");
+new userAccount(client, Discord).leveling({
+    channel: '1076598098043879575',//اي دي الشات
+    randomLetters: false, 
+    time: 10000, //الوقت 
+    type: 'eng' //اللغة 
+});
+client.login(process.env.token);
+
+const express = require("express");
+const app = express();
+app.listen(() => console.log("Server started"));
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
